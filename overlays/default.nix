d0ac2bc0e235b2eeb6809bc57b19e37b9c297c11
Finn Brandt <finn2003minicooper@gmail.com>
2026-07-01 21:02:35 +0200

    chore: update flake

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,13 +8,6 @@
 {
   nixpkgs.overlays = [
     inputs.nix-matlab.overlay
-    (final: prev: {
-      # Your own overlays...
-      static-configs = prev.callPackage ../config/static { };
-      obs-studio-plugins = prev.obs-studio-plugins // {
-        obs-wayland-hotkeys = prev.qt6Packages.callPackage ../packages/obs-wayland-hotkeys.nix { };
-      };
-    })
     (
       final: prev:
       let
