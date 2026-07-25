281f28240ff606008e966149eb31f35cba60bc71
Finn Brandt <finn2003minicooper@gmail.com>
2026-07-25 13:02:21 +0200

    fix: thread communication

--- hosts/rpi/default.nix
+++ hosts/rpi/default.nix
@@ -50,157 +50,40 @@
     ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="8c07", ENV{ID_MM_DEVICE_IGNORE}="1"
   '';
 
-  networking =
-    let
-      prefix = lib.strings.removeSuffix "\n" (builtins.readFile ../../secrets/ipv6Prefix.txt);
-    in
-    {
-      firewall.enable = false;
-      firewall.allowedTCPPorts = [
-        8123
-        8981
-      ];
-      dhcpcd = {
-        IPv6rs = false;
-        extraConfig = "noipv6";
-      };
-      networkmanager.unmanaged = [ "end0" ];
-      macvlans.ccu-shim = {
-        mode = "bridge";
-        interface = "end0";
-      };
-      interfaces.ccu-shim = {
-        useDHCP = false;
-        ipv4 = {
-          addresses = [
-            {
-              address = "192.168.178.6";
-              prefixLength = 32;
-            }
-          ];
-          routes = [
-            {
-              address = "192.168.178.7";
-              prefixLength = 32;
-              options = {
-                protocol = "static";
-              };
-            }
-          ];
-        };
-      };
-      interfaces.end0 = {
-        useDHCP = false;
-        ipv4 = {
-          addresses = [
-            {
-              address = "192.168.178.5";
-              prefixLength = 24;
-            }
-          ];
-          routes = [
-            {
-              address = "192.168.178.0";
-              prefixLength = 24;
-              options = {
-                mtu = "600";
-              };
-            }
-            {
-              address = "0.0.0.0";
-              prefixLength = 0;
-              via = "192.168.178.1";
-              options = {
-                mtu = "600";
-              };
-            }
-          ];
-        };
-        ipv6 = {
-          addresses = [
-            {
-              address = "${prefix}:cc7d:a1b1:b727:4780";
-              prefixLength = 64;
-            }
-            {
-              address = "fd97:58c:9bb1:0:cc7d:a1b1:b727:4781";
-              prefixLength = 64;
-            }
-            {
-              address = "fe80::cc7d:a1b1:b727:4782";
-              prefixLength = 64;
-            }
-          ];
-          routes = [
-            {
-              address = "fe80::";
-              prefixLength = 64;
-              options = {
-                mtu = "1024";
-              };
-            }
-            {
-              address = "fd97:58c:9bb1::";
-              prefixLength = 64;
-              options = {
-                mtu = "600";
-              };
-            }
-            {
-              address = "${prefix}::";
-              prefixLength = 64;
-              options = {
-                mtu = "600";
-              };
-            }
-            {
-              address = "fd97:58c:9bb1::";
-              prefixLength = 64;
-              via = "fe80::9a9b:cbff:febe:41eb";
-              options = {
-                mtu = "605";
-              };
-            }
-            {
-              address = "${prefix}::";
-              prefixLength = 56;
-              via = "fe80::9a9b:cbff:febe:41be";
-              options = {
-                mtu = "600";
-              };
-            }
-            {
-              address = "::";
-              prefixLength = 0;
-              via = "fe80::9a9b:cbff:febe:41eb";
-              options = {
-                mtu = "600";
-              };
-            }
-            {
-              address = "${prefix}:cc7d:a1b1:b727:4782";
-              prefixLength = 128;
-              options = {
-                mtu = "600";
-              };
-            }
-          ];
-        };
-      };
-      defaultGateway = {
-        address = "192.168.178.1";
-        interface = "end0";
-      };
-      defaultGateway6 = {
-        address = "fe80::9a9b:cbff:febe:41eb";
-        interface = "end0";
-      };
-      nameservers = [
-        "192.168.178.1"
-        "fd97:58c:9bb1:0:9a9b:cbff:febe:41eb"
-        "${prefix}:9a9b:cbff:febe:41eb"
-      ];
+  networking = {
+    firewall.enable = false;
+    firewall.allowedTCPPorts = [
+      8123
+      8981
+    ];
+    macvlans.ccu-shim = {
+      mode = "bridge";
+      interface = "end0";
     };
+    interfaces.ccu-shim = {
+      useDHCP = false;
+      ipv4 = {
+        addresses = [
+          {
+            address = "192.168.178.6";
+            prefixLength = 32;
+          }
+        ];
+        routes = [
+          {
+            address = "192.168.178.7";
+            prefixLength = 32;
+            options = {
+              protocol = "static";
+            };
+          }
+        ];
+      };
+    };
+    interfaces.end0 = {
+      useDHCP = true;
+    };
+  };
 
   hardware.bluetooth = {
     enable = true;
